933662b338b32b21c2452f9849704264659e855f
Pampa Nelson <pampa.nelson@gmail.com>
2021-01-16 07:35:10 +0800

    add streaming load wasm without js

--- src/components/Wasm.vue
+++ src/components/Wasm.vue
@@ -2,6 +2,7 @@
   <div>
     <div>{{ result }}</div>
     <p button @click="callAdd(3, 2)">Add!</p>
+    <p button @click="streamingCallAdd(3, 5)">Streaming load Add</p>
   </div>
 </template>
 
@@ -10,20 +11,33 @@ import Module from "../wasm/func";
 // put related .wasm file into /public
 // or change url in func.js as var wasmBinaryFile = "wasm/func.wasm";
 
-let instance = {
+let promiseInstance = {
   ready: new Promise((resolve) => {
-    console.log("wasm loading");
+    console.log("wasm loading"); // loading callback
     Module({
       onRuntimeInitialized() {
-        instance = Object.assign(this, {
+        promiseInstance = Object.assign(this, {
           ready: Promise.resolve(),
         });
         resolve();
-        console.log("wasm loaded");
+        console.log("wasm loaded"); // loaded callback
       },
     });
   }),
 };
+
+let streamingInstance;
+let bStreamingLoaded = false;
+(async () => {
+  const fetchPromise = fetch("func.wasm");
+  console.log("streaming loading");
+  let { instance } = await WebAssembly.instantiateStreaming(fetchPromise);
+  streamingInstance = instance;
+  console.log("streaming loaded");
+  // const result = streamingInstance.exports.add(4, 2);
+  // console.log(result);
+  bStreamingLoaded = true;
+})();
 
 export default {
   name: "Wasm",
@@ -35,7 +49,15 @@ export default {
   },
   methods: {
     callAdd(a, b) {
-      instance.ready.then(() => (this.result = instance._add(a, b)));
+      promiseInstance.ready.then(
+        () => (this.result = promiseInstance._add(a, b))
+      );
+    },
+
+    streamingCallAdd(a, b) {
+      if (bStreamingLoaded) {
+        this.result = streamingInstance.exports.add(a, b);
+      }
     },
   },
 };
